ba5a3f931c28c2d3e7c5d84633d4681e51a1d52e
Muhammed Sari <muhammed@dive.be>
2022-04-03 09:35:30 +0200

    Add config publishing

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,14 +8,30 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 final class ServiceProvider extends BaseServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->registerConfig();
+        }
+    }
+
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/dry-requests.php', 'dry-requests');
 
-        $this->callAfterResolving(ExceptionHandler::class, $this->registerWith(...));
+        $this->callAfterResolving(ExceptionHandler::class, $this->registerException(...));
     }
 
-    private function registerWith(Handler $handler)
+    private function registerConfig()
+    {
+        $config = 'dry-requests.php';
+
+        $this->publishes([
+            __DIR__ . '/../config/' . $config => $this->app->configPath($config),
+        ], 'config');
+    }
+
+    private function registerException(Handler $handler)
     {
         $handler->ignore(SucceededException::class);
         $handler->renderable(fn (SucceededException $e) => $this->app[Responder::class]->respond());
